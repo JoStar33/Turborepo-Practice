98ee5a0b129a36987a225de94458e900c55f37ff
HoCG <rhwkd5012@naver.com>
2024-09-08 16:30:17 +0900

    update: eslint설정

--- apps/test1/src/App.tsx
+++ apps/test1/src/App.tsx
@@ -1,12 +1,19 @@
 // 별도로 라이브러리를 받아 사용하는것 또한 가능하다. 이때에는 원하는 경로로가서 yarn add를 해주면 됨.
 // 이때, 그 라이브러리는 test2에서만 유효!
+import React from 'react';
 import { FaAccusoft } from 'react-icons/fa';
 
 export default function App() {
-	return (
-		<div>
-			HI
-			<FaAccusoft size={30} />
-		</div>
-	);
+  const [value, setValue] = React.useState(1234);
+
+  React.useEffect(() => {
+    setValue(value);
+  }, []);
+
+  return (
+    <div>
+      HI
+      <FaAccusoft size={30} />
+    </div>
+  );
 }
--- packages/eslint-config/react-internal.js
+++ packages/eslint-config/react-internal.js
@@ -1,6 +1,6 @@
-const { resolve } = require("node:path");
+const { resolve } = require('node:path');
 
-const project = resolve(process.cwd(), "tsconfig.json");
+const project = resolve(process.cwd(), 'tsconfig.json');
 
 /*
  * This is a custom ESLint configuration for use with
@@ -10,17 +10,25 @@ const project = resolve(process.cwd(), "tsconfig.json");
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["eslint:recommended", "prettier", "turbo"],
-  plugins: ["only-warn"],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'turbo',
+  ],
+  plugins: ['react-hooks', 'import', 'only-warn', 'react-refresh'],
+  parser: '@typescript-eslint/parser',
   globals: {
     React: true,
     JSX: true,
   },
+  rules: { 'react-hooks/rules-of-hooks': 'warn', 'react-hooks/exhaustive-deps': 'warn' },
   env: {
     browser: true,
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       typescript: {
         project,
       },
@@ -28,12 +36,12 @@ module.exports = {
   },
   ignorePatterns: [
     // Ignore dotfiles
-    ".*.js",
-    "node_modules/",
-    "dist/",
+    '.*.js',
+    'node_modules/',
+    'dist/',
   ],
   overrides: [
     // Force ESLint to detect .tsx files
-    { files: ["*.js?(x)", "*.ts?(x)"] },
+    { files: ['*.js?(x)', '*.ts?(x)'] },
   ],
 };
