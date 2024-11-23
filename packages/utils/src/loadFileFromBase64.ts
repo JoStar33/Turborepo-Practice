528f617b8fcf9980837bc8ab29ffd6b1c80719c7
HoCG <rhwkd5012@naver.com>
2024-11-23 18:20:50 +0900

    update: 터보레포 유틸패키지에 공통 타입모듈 임포트하도록 수정 및 공부내용 기록

--- packages/utils/src/loadFileFromBase64.ts
+++ packages/utils/src/loadFileFromBase64.ts
@@ -1,15 +1,24 @@
 export default function loadFileFromBase64(base64String: string) {
-	if (!base64String) {
-		return null;
-	}
-	const byteString = atob(base64String.split(',')[1]);
-	const mimeString = base64String.split(',')[0].split(':')[1].split(';')[0];
-	const ab = new ArrayBuffer(byteString.length);
-	const ia = new Uint8Array(ab);
-	for (let i = 0; i < byteString.length; i++) {
-		ia[i] = byteString.charCodeAt(i);
-	}
-	const blob = new Blob([ab], { type: mimeString });
-	console.log(new File([blob], 'PDF_파일.pdf', { type: blob.type }));
-	return new File([blob], 'PDF_파일.pdf', { type: blob.type });
+  if (!base64String) {
+    return null;
+  }
+  const byteExtractPrevStringFirst = base64String.split(',')[1];
+  const byteExtractPrevStringSecond = base64String.split(',')[0];
+  if (!byteExtractPrevStringSecond) {
+    return null;
+  }
+  const mimeExtractPrevString = byteExtractPrevStringSecond.split(':')[1];
+  if (!byteExtractPrevStringFirst || !mimeExtractPrevString) {
+    return null;
+  }
+  const byteString = atob(byteExtractPrevStringFirst);
+  const mimeString = mimeExtractPrevString.split(';')[0];
+  const ab = new ArrayBuffer(byteString.length);
+  const ia = new Uint8Array(ab);
+  for (let i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+  const blob = new Blob([ab], { type: mimeString });
+  console.log(new File([blob], 'PDF_파일.pdf', { type: blob.type }));
+  return new File([blob], 'PDF_파일.pdf', { type: blob.type });
 }
